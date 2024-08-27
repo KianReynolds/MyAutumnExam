064e0d84e765185216a1ef963a93d89abb7a874a
Kian Reynolds <S00237055@atu.ie>
2024-08-27 09:59:30 +0100

    created a Console App

--- MyRepeatExam/Booking.cs
+++ MyRepeatExam/Booking.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace MyRepeatExam
 {
@@ -16,11 +18,11 @@ namespace MyRepeatExam
         //foreign key
         public int KennelID { get; set; }
         //navigation between the 2 classes
-        public virtual Kennel Kennel  { get; set; }
+        public virtual Kennel Kennel { get; set; }
 
         public Booking()
         {
-                Kennel = new Kennel();
+            Kennel = new Kennel();
         }
 
     }
@@ -28,16 +30,17 @@ namespace MyRepeatExam
     public class Kennel
     {
         public int KennelID { get; set; }
-        public Enum KennelType["Basic", "StandardCommands", "Premium"]
+        //public Enum KennelType {"Basic", "StandardCommands", "Premium"}
         public string Location { get; set; }
-        
+
         public virtual List<Booking> Bookings { get; set; }
 
     }
 
     public class KennelData : DbContext
     {
-        public KennelData(string databaseName): base(databaseName) { }    
-        public DBSet<Booking> Bookings { get; set;}
+        public KennelData(string databaseName) : base(databaseName) { }
+        public DbSet<Booking> Bookings { get; set; }
         public DbSet<Kennel> Kennels { get; set; }
     }
+}
